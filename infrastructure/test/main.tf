fc51bcaed43c5560a7d1b37902793986ff8f6c1b
Andreas Isnes <andreas.isnes@gmail.com>
2024-06-18 14:17:15 +0200

    add test tf project

---
+++ infrastructure/test/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.48.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "azurerm" {
+    use_azuread_auth = true
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+provider "azurerm" {
+  features {} # Required
+}
+
+# https://registry.terraform.io/providers/integrations/github/latest/docs
+provider "github" {
+  owner = local.configuration.admin.github.owner
+  app_auth {} # Required
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "altinnkake"
+  location = "norwayeast"
+}
