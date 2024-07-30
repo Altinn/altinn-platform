e4480638e4b2c778116d1e4dd026c61882bacd72
Sebastian Duran <duran.c.sebastian@gmail.com>
2024-07-30 11:39:31 +0200

    fix default workdir

--- tools/daisctl/pkg/kube/app_test.go
+++ tools/daisctl/pkg/kube/app_test.go
@@ -64,8 +64,9 @@ func TestGetAppInfos(t *testing.T) {
 			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(tt.statusCode)
+				// check return value, otherwise lint will complain
 				if _, err := w.Write([]byte(tt.mockResponse)); err != nil {
-					t.Errorf("failed to write response: %v", err)
+					t.Errorf("failed to write mock response: %v", err)
 				}
 			}))
 			defer server.Close()
