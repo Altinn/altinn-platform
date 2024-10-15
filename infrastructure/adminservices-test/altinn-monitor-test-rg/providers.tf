2df75a0bf9a5b7f810a99cac02d612988b1e99c7
Bengt Fredh <bengt.rino.fredh@digdir.no>
2024-10-15 15:06:25 +0200

    Add github actions

--- infrastructure/adminservices-test/altinn-monitor-test-rg/providers.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/providers.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = data.azurerm_subscription.subscription_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
   features {}
   resource_providers_to_register = [
     "Microsoft.Monitor",
