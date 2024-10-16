bc8c6d50ab39d6d599a689cd7ae26d829dada67b
Bengt <32734566+bengtfredh@users.noreply.github.com>
2024-10-16 09:54:30 +0200

    iac test central monitoring (#1010)
    * add iac for test central monitoring
    
    * Add .github/workflows/altinn-monitor-test-rg-deploy.yml
    
    * Run with terraform version : latest
    
    Co-authored-by: Vemund Gaukstad <tjololo@users.noreply.github.com>
    
    * Run with env test
    
    ---------
    
    Co-authored-by: Bengt Fredh <bengt.rino.fredh@digdir.no>
    Co-authored-by: Vemund Gaukstad <tjololo@users.noreply.github.com>

---
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/grafana.tf
@@ -0,0 +1,46 @@
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                              = "altinn-grafana-test"
+  resource_group_name               = azurerm_resource_group.rg.name
+  location                          = azurerm_resource_group.rg.location
+  grafana_major_version             = 10
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  azure_monitor_workspace_integrations {
+    resource_id = azurerm_monitor_workspace.altinn_monitor.id
+  }
+}
+
+resource "azurerm_role_assignment" "tf_grafana_admin" {
+  scope                            = azurerm_dashboard_grafana.grafana.id
+  role_definition_name             = "Grafana Admin"
+  principal_id                     = data.azurerm_client_config.current.object_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "monitoring_reader_rg" {
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_id               = "/subscriptions/${split("/", azurerm_monitor_workspace.altinn_monitor.id)[2]}/providers/Microsoft.Authorization/roleDefinitions/43d0d8ad-25c7-4714-9337-8ba259a9fe05"
+  principal_id                     = azurerm_dashboard_grafana.grafana.identity[0].principal_id
+  skip_service_principal_aad_check = true
+}
+
+locals {
+  altinn_30_operations      = "143ed28a-6e6d-4ca0-8273-eecb9c1665ba"
+  altinn_30_operations_prod = "5a5ed585-9f7c-4b94-80af-9ceee8124db3"
+  grafana_admin             = [local.altinn_30_operations, local.altinn_30_operations_prod]
+}
+
+resource "azurerm_role_assignment" "grafana_admin" {
+  for_each                         = { for value in local.grafana_admin : value => value if value != null }
+  scope                            = azurerm_dashboard_grafana.grafana.id
+  role_definition_name             = "Grafana Admin"
+  principal_id                     = each.key
+  principal_type                   = "Group"
+  skip_service_principal_aad_check = true
+}
---
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/rg.tf
@@ -0,0 +1,40 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "altinn-monitor-test-rg"
+  location = "norwayeast"
+}
+
+resource "azurerm_role_assignment" "altinn_apps_terraform" {
+  for_each                         = toset(["Contributor", "User Access Administrator"])
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = each.key
+  principal_id                     = "26e1eede-69a5-46b2-abea-f839092273ba"
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "platform_terraform" {
+  for_each                         = toset(["Contributor", "User Access Administrator"])
+  scope                            = azurerm_resource_group.rg.id
+  role_definition_name             = each.key
+  principal_id                     = "641fc568-3e2f-4174-a7ce-d91f50c8e6d6"
+  skip_service_principal_aad_check = true
+}
+
+locals {
+  parsed_endpoint_id = provider::azurerm::parse_resource_id("${azurerm_monitor_workspace.altinn_monitor.default_data_collection_endpoint_id}")
+}
+
+resource "azurerm_role_assignment" "altinn_apps_terraform_ma_rg" {
+  for_each                         = toset(["Contributor", "User Access Administrator"])
+  scope                            = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.parsed_endpoint_id["resource_group_name"]}"
+  role_definition_name             = each.key
+  principal_id                     = "26e1eede-69a5-46b2-abea-f839092273ba"
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "platform_terraform_ma_rg" {
+  for_each                         = toset(["Contributor", "User Access Administrator"])
+  scope                            = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.parsed_endpoint_id["resource_group_name"]}"
+  role_definition_name             = each.key
+  principal_id                     = "641fc568-3e2f-4174-a7ce-d91f50c8e6d6"
+  skip_service_principal_aad_check = true
+}
