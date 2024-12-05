8195844ed36a0aeb3b64d3aeeb4c0ecfb2d2b628
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2024-12-05 14:53:00 +0100

    deploy the k6 operator

---
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_k6_operator.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "k6_operator" {
+  name             = "k6-operator"
+  namespace        = "k6-operator-system"
+  create_namespace = true
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana/k6-operator"
+  version          = "3.10.1"
+}
