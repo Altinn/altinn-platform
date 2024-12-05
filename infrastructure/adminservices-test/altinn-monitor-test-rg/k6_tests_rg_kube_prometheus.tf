c6d362c15fb553a517f8314720b7d4aaa0446cc7
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2024-12-05 14:25:53 +0100

    deploy kube prometheus stack

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_kube_prometheus.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_kube_prometheus.tf
@@ -4,3 +4,37 @@ resource "helm_release" "prometheus_operator_crds" {
   chart      = "prometheus-operator-crds"
   version    = "16.0.1"
 }
+
+resource "helm_release" "kube_prometheus_stack" {
+  depends_on       = [helm_release.prometheus_operator_crds]
+  name             = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  skip_crds        = true
+  version          = "66.3.1"
+
+  values = [<<-EOT
+              crds:
+                enabled: false
+              alertmanager:
+                enabled: true
+              grafana:
+                enabled: false
+              prometheus:
+                enabled: true
+                prometheusSpec:
+                  externalLabels:
+                    cluster: "${azurerm_kubernetes_cluster.k6tests.name}"
+                  priorityClassName: "system-cluster-critical"
+                  retention: 1d
+                  storageSpec:
+                    volumeClaimTemplate:
+                      spec:
+                        resources:
+                          requests:
+                            storage: 5Gi
+              EOT
+  ]
+}
