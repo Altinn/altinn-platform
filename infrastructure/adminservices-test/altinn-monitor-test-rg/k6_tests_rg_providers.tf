b11b7752cfa16115419d8c9b0bdac38fc0e4795e
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2024-12-05 13:02:05 +0100

    feat: create a k8s cluster with two different node pools to run k6 tests from

---
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_providers.tf
@@ -0,0 +1,24 @@
+/*
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  backend "azurerm" {
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "1ce8e9af-c2d6-44e7-9c5e-099a308056fe"
+  features {}
+  resource_providers_to_register = [
+    "Microsoft.Monitor",
+    "Microsoft.AlertsManagement",
+    "Microsoft.Dashboard",
+    "Microsoft.KubernetesConfiguration"
+  ]
+}
+*/
