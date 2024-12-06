f383817a5cbf437b389cc616bd4be57f1f103695
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2024-12-06 09:50:48 +0100

    Azure configuration for WI

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_kube_prometheus.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_kube_prometheus.tf
@@ -20,3 +20,26 @@ resource "helm_release" "kube_prometheus_stack" {
     cluster_name = "${azurerm_kubernetes_cluster.k6tests.name}" })}"
   ]
 }
+
+resource "azuread_application" "prometheus" {
+  display_name     = "adminservicestest-k6tests-prometheus"
+  sign_in_audience = "AzureADMyOrg"
+}
+
+resource "azuread_service_principal" "prometheus" {
+  client_id = azuread_application.prometheus.client_id
+}
+
+resource "azuread_application_federated_identity_credential" "prometheus" {
+  application_id = azuread_application.prometheus.id
+  display_name   = "adminservicestest-k6tests-prometheus"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = azurerm_kubernetes_cluster.k6tests.oidc_issuer_url
+  subject        = "system:serviceaccount:monitoring:kube-prometheus-stack-prometheus"
+}
+
+resource "azurerm_role_assignment" "monitoring_metrics_publisher" {
+  scope                = azurerm_monitor_workspace.k6tests_amw.default_data_collection_endpoint_id
+  role_definition_name = "Monitoring Metrics Publisher"
+  principal_id         = azuread_service_principal.prometheus.id
+}
