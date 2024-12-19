17b58a5972c50dca9062fbdb1744972eb8531ae5
Sebastian Duran <duran.c.sebastian@gmail.com>
2024-12-19 12:04:28 +0100

    typo in location for rf

--- infrastructure/adminservices-test/altinn-monitor-test-rg/data.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/data.tf
@@ -3,7 +3,6 @@ data "azurerm_client_config" "current" {}
 # Dialogporten
 data "azurerm_resource_group" "rg_dp_test" {
   name     = "dp-be-test-rg"
-  location = "norwayeast"
 }
 
 data "azurerm_log_analytics_workspace" "dp_law_test" {
