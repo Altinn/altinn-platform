aed7cf751af1913567fd62aee6490be723541873
Sebastian Duran <duran.c.sebastian@gmail.com>
2024-12-19 13:44:44 +0100

    feat(grafana): add dialogporten test (#1192)
    * feat(grafana): add dialogporten test

--- infrastructure/adminservices-test/altinn-monitor-test-rg/data.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/data.tf
@@ -1,1 +1,13 @@
 data "azurerm_client_config" "current" {}
+
+# Dialogporten
+data "azurerm_resource_group" "rg_dp_test" {
+  name     = "dp-be-test-rg"
+  provider = azurerm.dp
+}
+
+data "azurerm_log_analytics_workspace" "dp_law_test" {
+  name                = "dp-be-test-insightsWorkspace"
+  resource_group_name = data.azurerm_resource_group.rg_dp_test.name
+  provider = azurerm.dp
+}
