3bf27c33848dff4f50059fd4748175245b232e17
Sebastian Duran <duran.c.sebastian@gmail.com>
2025-01-13 23:11:20 +0100

    fix(grafana): add the rest of envs from DP (#1215)
    * fix(grafana): add the rest of envs from DP
    
    * fmt

--- infrastructure/adminservices-test/altinn-monitor-test-rg/data.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/data.tf
@@ -1,13 +1,39 @@
 data "azurerm_client_config" "current" {}
 
 # Dialogporten
-data "azurerm_resource_group" "rg_dp_test" {
-  name     = "dp-be-test-rg"
-  provider = azurerm.dp
+data "azurerm_resource_group" "rg_dp_stag" {
+  name     = "dp-be-staging-rg"
+  provider = azurerm.dp_stag
+}
+
+data "azurerm_resource_group" "rg_dp_prod" {
+  name     = "dp-be-prod-rg"
+  provider = azurerm.dp_prod
+}
+
+data "azurerm_resource_group" "rd_dp_test" {
+  for_each = toset(values(var.insights_workspace_test_dp))
+  name     = each.value
+  provider = azurerm.dp_test
 }
 
 data "azurerm_log_analytics_workspace" "dp_law_test" {
-  name                = "dp-be-test-insightsWorkspace"
-  resource_group_name = data.azurerm_resource_group.rg_dp_test.name
-  provider            = azurerm.dp
+  for_each = var.insights_workspace_test_dp
+
+  name                = each.key
+  resource_group_name = each.value
+  provider            = azurerm.dp_test
 }
+
+data "azurerm_log_analytics_workspace" "dp_law_stag" {
+  name                = "dp-be-staging-insightsWorkspace"
+  resource_group_name = data.azurerm_resource_group.rg_dp_stag.name
+  provider            = azurerm.dp_stag
+}
+
+data "azurerm_log_analytics_workspace" "dp_law_prod" {
+  name                = "dp-be-prod-insightsWorkspace"
+  resource_group_name = data.azurerm_resource_group.rg_dp_prod.name
+  provider            = azurerm.dp_prod
+}
+
