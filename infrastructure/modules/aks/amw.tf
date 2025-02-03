739b6856b8998ccd86794a4a68179023d66e207b
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-02-03 12:21:26 +0100

    Add prom collection rules auth-at22-aks

--- infrastructure/modules/aks/amw.tf
+++ infrastructure/modules/aks/amw.tf
@@ -1,15 +1,15 @@
-resource "azurerm_monitor_data_collection_endpoint" "aks" {
+resource "azurerm_monitor_data_collection_endpoint" "amw" {
   name                = "${azurerm_kubernetes_cluster.aks.name}}-mdce"
   resource_group_name = azurerm_resource_group.aks.name
   location            = azurerm_resource_group.aks.location
   kind                = "Linux"
 }
 
-resource "azurerm_monitor_data_collection_rule" "aks" {
+resource "azurerm_monitor_data_collection_rule" "amw" {
   name                        = "${azurerm_kubernetes_cluster.aks.name}}-mdcr"
   resource_group_name         = azurerm_resource_group.aks.name
   location                    = azurerm_resource_group.aks.location
-  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.aks.id
+  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.amw.id
   kind                        = "Linux"
 
   destinations {
@@ -38,10 +38,10 @@ resource "azurerm_monitor_data_collection_rule" "aks" {
   ]
 }
 
-resource "azurerm_monitor_data_collection_rule_association" "aks" {
+resource "azurerm_monitor_data_collection_rule_association" "amw" {
   name                    = "${azurerm_kubernetes_cluster.aks.name}}-mdcra"
   target_resource_id      = azurerm_kubernetes_cluster.aks.id
-  data_collection_rule_id = azurerm_monitor_data_collection_rule.aks.id
+  data_collection_rule_id = azurerm_monitor_data_collection_rule.amw.id
   description             = "Association of data collection rule. Deleting this association will break the data collection for this AKS Cluster."
   depends_on = [
     azurerm_monitor_data_collection_rule.aks
