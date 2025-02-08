10165c5b76c0871b86b47be156f0aaef0c322c45
Bengt <bengt@fredh.com>
2025-02-08 12:07:42 +0100

    Update auth-at22-aks subnets

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/aks.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/aks.tf
@@ -11,8 +11,8 @@ module "aks" {
     "fd0a:7204:c37f:900::/56"
   ]
   subnet_address_prefixes = {
-    aks_syspool  = ["fdb8:8b5f:00f0:901::/64", "10.202.72.0/24"]
-    aks_workpool = ["fdb8:8b5f:00f0:902::/64", "10.202.73.0/24"]
+    aks_syspool  = ["fd0a:7204:c37f:901::/64", "10.202.72.0/24"]
+    aks_workpool = ["fd0a:7204:c37f:902::/64", "10.202.73.0/24"]
   }
   pool_configs = {
     syspool = {
