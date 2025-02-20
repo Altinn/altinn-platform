3a90f371e005147cf0da4448ab0cd45c5cc4fd45
tjololo <1145298+tjololo@users.noreply.github.com>
2025-02-20 09:41:02 +0100

    install powershell on terraform agent

--- infrastructure/images/terraform-azure-devops-agent/scripts/install.sh
+++ infrastructure/images/terraform-azure-devops-agent/scripts/install.sh
@@ -4,7 +4,9 @@ set -e
 # Versions
 KUBECTL_VERSION="v1.32.1" #Get the latest version with: curl -L -s https://dl.k8s.io/release/stable.txt
 
-# Install kubectl
+###################################
+### Install kubectl
+###################################
 echo "Installing kubectl $KUBECTL_VERSION"
 curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl.sha256"
@@ -14,3 +16,31 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # Clean up
 rm kubectl kubectl.sha256 install.sh
+
+###################################
+### Install powershell
+###################################
+# Install dependencies
+apt-get update
+apt-get install -y wget
+# Get the version of Ubuntu
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+
+# Register the Microsoft repository keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+
+###################################
+# Install PowerShell
+sudo apt-get install -y powershell
+
+# remove apt cache
+rm -rf /var/lib/apt/lists/*
