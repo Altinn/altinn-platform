4846ddf0974b77a8a4ccb79eb140e94a24b95e42
tjololo <1145298+tjololo@users.noreply.github.com>
2025-02-20 10:43:37 +0100

    feat: install az powershell module and helm

--- infrastructure/images/terraform-azure-devops-agent/scripts/install.sh
+++ infrastructure/images/terraform-azure-devops-agent/scripts/install.sh
@@ -3,6 +3,7 @@ set -e
 
 # Versions
 KUBECTL_VERSION="v1.32.1" #Get the latest version with: curl -L -s https://dl.k8s.io/release/stable.txt
+HELM_VERSION="v3.17.0" #Find the latest version at https://github.com/helm/helm/releases
 
 ###################################
 ### Install kubectl
@@ -15,7 +16,7 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 # Install kubectl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # Clean up
-rm kubectl kubectl.sha256 install.sh
+rm kubectl kubectl.sha256
 
 ###################################
 ### Install powershell
@@ -41,6 +42,25 @@ apt-get update
 ###################################
 # Install PowerShell
 apt-get install -y powershell
-
+# Install the Azure PowerShell module
+Install-Module -Name Az -Repository PSGallery -Force
 # remove apt cache
 rm -rf /var/lib/apt/lists/*
+
+
+###################################
+# Install Helm
+###################################
+curl -LO "https://github.com/helm/helm/releases/download/${HELM_VERSION}/helm-${HELM_VERSION}-linux-amd64.tar.gz.asc"
+curl -LO "https://github.com/helm/helm/releases/download/${HELM_VERSION}/helm-${HELM_VERSION}-linux-386.tar.gz.sha256sum.asc"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+
+rm -rf helm-${HELM_VERSION}-linux-amd64.tar.gz.asc helm-${HELM_VERSION}-linux-386.tar.gz.sha256sum.asc linux-amd64
+
+
+###################################
+# Remove self
+###################################
+rm install.sh
