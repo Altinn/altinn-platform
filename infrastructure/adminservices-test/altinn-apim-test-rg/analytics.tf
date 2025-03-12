752a47531e68c0fc33b4fe99822477b49ee068e1
Vemund Gaukstad <tjololo@users.noreply.github.com>
2025-03-12 10:47:55 +0100

    feat(apim): remove unsupported and add law (#1425)
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

--- infrastructure/adminservices-test/altinn-apim-test-rg/analytics.tf
+++ infrastructure/adminservices-test/altinn-apim-test-rg/analytics.tf
@@ -15,6 +15,18 @@ resource "azurerm_api_management_logger" "apimlogger" {
   }
 }
 
+resource "azurerm_monitor_diagnostic_setting" "apimdiagnostics_settings" {
+  name                       = "${var.name_prefix}-apimdiagnostics-settings"
+  target_resource_id         = azurerm_api_management.admin_test_apim.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.apimlogs.id
+  enabled_log {
+    category = "GatewayLogs"
+  }
+  metric {
+    category = "AllMetrics"
+  }
+}
+
 resource "azurerm_api_management_diagnostic" "application_insights" {
   identifier               = "applicationinsights"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -64,24 +76,10 @@ resource "azurerm_api_management_diagnostic" "application_insights" {
   }
 }
 
-
-resource "azurerm_api_management_diagnostic" "azuremonitor" {
-  identifier               = "azuremonitor"
-  resource_group_name      = azurerm_resource_group.rg.name
-  api_management_name      = azurerm_api_management.admin_test_apim.name
-  api_management_logger_id = azurerm_api_management_logger.apimlogger.id
-
-  sampling_percentage       = 0.0
-  always_log_errors         = true
-  log_client_ip             = true
-  verbosity                 = "information"
-  http_correlation_protocol = "W3C"
-
-  frontend_request {}
-
-  frontend_response {}
-
-  backend_request {}
-
-  backend_response {}
+resource "azurerm_log_analytics_workspace" "apimlogs" {
+  name                = "${var.name_prefix}-apimlogs"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 7
 }
