d4d7c9543efd4774be56efc2c752f294974fedd2
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-03-13 13:55:11 +0100

    Order variables alphabetical

--- infrastructure/modules/aks/variables.tf
+++ infrastructure/modules/aks/variables.tf
@@ -1,37 +1,39 @@
-variable "prefix" {
-  type        = string
-  description = "Prefix for resource names"
+variable "admin_group_object_ids" {
+  type        = list(string)
+  description = "List of group object IDs to get admin access to the cluster"
 }
+
+variable "aks_acrpull_scopes" {
+  type        = list(string)
+  description = "List of AKS ACR pull scopes"
+  validation {
+    condition     = length(var.aks_acrpull_scopes) > 0
+    error_message = "You must provide at least one ACR resource ID."
+  }
+}
+
+variable "aks_sku_tier" {
+  type        = string
+  default     = "Free"
+  description = "Kubernetes SKU"
+}
+
 variable "environment" {
   type        = string
   description = "Environment for resources"
 }
-variable "subscription_id" {
+
+variable "kubernetes_version" {
   type        = string
-  description = "Subscription id to deploy services"
+  description = "Kubernetes version"
 }
+
 variable "location" {
   type        = string
   default     = "norwayeast"
   description = "Default region for resources"
 }
-variable "aks_sku_tier" {
-  type        = string
-  default     = "Free"
-  description = "Kubernetes sku"
-}
-variable "kubernetes_version" {
-  type        = string
-  description = "Kubernetes version"
-}
-variable "vnet_address_space" {
-  type        = list(string)
-  description = "vnet adress space"
-}
-variable "subnet_address_prefixes" {
-  type        = map(list(string))
-  description = "list of subnets"
-}
+
 variable "pool_configs" {
   type = map(object({
     vm_size              = string
@@ -40,19 +42,27 @@ variable "pool_configs" {
     min_count            = number
     max_count            = number
   }))
-  description = "variables for nodepools"
+  description = "Variables for node pools"
 }
-variable "aks_acrpull_scopes" {
-  type        = list(string)
-  description = "List of aks acrpull scopes"
-  validation {
-    condition     = length(var.aks_acrpull_scopes) > 0
-    error_message = "You must provide at least one ACR resource ID."
-  }
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for resource names"
 }
-variable "admin_group_object_ids" {
+
+variable "subscription_id" {
+  type        = string
+  description = "Subscription ID to deploy services"
+}
+
+variable "subnet_address_prefixes" {
+  type        = map(list(string))
+  description = "List of subnets"
+}
+
+variable "vnet_address_space" {
   type        = list(string)
-  description = "List og group abject ids to get admin access cluster"
+  description = "VNet address space"
 }
 
 # Optional explicit variables to override values derived from prefix and environment
@@ -61,53 +71,63 @@ variable "azurerm_kubernetes_cluster_aks_name" {
   default     = ""
   description = "Optional explicit name of the AKS cluster"
 }
-variable "azurerm_resource_group_aks_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the AKS resource group"
-}
-variable "azurerm_resource_group_monitor_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the monitor resource group"
-}
-variable "azurerm_virtual_network_aks_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the AKS virtual network"
-}
-variable "azurerm_virtual_public_ip_pip4_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the public ipv4"
-}
-variable "azurerm_virtual_public_ip_pip6_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the public ipv6"
-}
-variable "azurerm_public_ip_prefix_prefix4_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the public ipv4 prefix"
-}
-variable "azurerm_public_ip_prefix_prefix6_name" {
-  type        = string
-  default     = ""
-  description = "Optional explicit name of the public ipv6 prefix"
-}
+
 variable "azurerm_log_analytics_workspace_aks_name" {
   type        = string
   default     = ""
   description = "Optional explicit name of the log analytics workspace"
 }
+
 variable "azurerm_monitor_workspace_aks_name" {
   type        = string
   default     = ""
   description = "Optional explicit name of the monitor workspace"
 }
+
+variable "azurerm_public_ip_prefix_prefix4_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the public ipv4 prefix"
+}
+
+variable "azurerm_public_ip_prefix_prefix6_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the public ipv6 prefix"
+}
+
+variable "azurerm_resource_group_aks_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the AKS resource group"
+}
+
+variable "azurerm_resource_group_monitor_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the monitor resource group"
+}
+
 variable "azurerm_storage_account_aks_name" {
   type        = string
   default     = ""
   description = "Optional explicit name of the AKS Log storage account"
+}
+
+variable "azurerm_virtual_network_aks_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the AKS virtual network"
+}
+
+variable "azurerm_virtual_public_ip_pip4_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the public ipv4"
+}
+
+variable "azurerm_virtual_public_ip_pip6_name" {
+  type        = string
+  default     = ""
+  description = "Optional explicit name of the public ipv6"
 }
