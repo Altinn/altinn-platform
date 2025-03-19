5569aa455ac3291b353622cf5d629c50168c2b3c
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-03-19 12:40:42 +0100

    Add more options to aks module output

--- infrastructure/modules/aks/output.tf
+++ infrastructure/modules/aks/output.tf
@@ -1,21 +1,31 @@
+output "aks_identity" {
+  value = azurerm_kubernetes_cluster.aks.identity
+}
+
+output "aks_kubelet_identity" {
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
 output "aks_node_resource_group" {
   value = azurerm_kubernetes_cluster.aks.node_resource_group
 }
+
+output "aks_oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+}
+
+output "kube_admin_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config
+}
+
 output "pip4_ip_address" {
   value = azurerm_public_ip.pip4.ip_address
 }
+
 output "pip6_ip_address" {
   value = azurerm_public_ip.pip6.ip_address
-}
-output "aks_kube_config_client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate
-}
-output "aks_kube_config_client_key" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key
-}
-output "aks_kube_config_cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate
-}
-output "aks_kube_config_host" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
 }
