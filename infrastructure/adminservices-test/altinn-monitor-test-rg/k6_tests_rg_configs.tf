cc6d9e6805a40009b30224c3720f0c0ac0877be3
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-03-27 21:50:37 +0100

    remove configmaps

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_configs.tf
+++
@@ -1,4 +0,0 @@
-resource "kubernetes_manifest" "deploy_environments_manifests" {
-  for_each = fileset("deploy-environments-manifests/", "*.yaml")
-  manifest = yamldecode(file("deploy-environments-manifests/${each.value}"))
-}
