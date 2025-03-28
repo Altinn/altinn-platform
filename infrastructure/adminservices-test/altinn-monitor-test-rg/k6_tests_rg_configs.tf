6e3866beba56f5227eecf1708ea8039110d4a2a4
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-03-28 10:24:28 +0100

    fix: domain names (#1488)

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_configs.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_configs.tf
@@ -5,14 +5,38 @@ locals {
       in var.k8s_rbac :
       v["namespace"]
   ])
+
   deploy_envs = [
-    "at21",
-    "at22",
-    "at23",
-    "at24",
-    "tt02",
-    "yt01",
-    "prod",
+    {
+      name : "at22",
+      env_type : "dev",
+      suffix : "cloud"
+    },
+    {
+      name : "at23",
+      env_type : "dev",
+      suffix : "cloud"
+    },
+    {
+      name : "at24",
+      env_type : "dev",
+      suffix : "cloud"
+    },
+    {
+      name : "yt01",
+      env_type : "perf",
+      suffix : "cloud"
+    },
+    {
+      name : "tt02",
+      env_type : "staging",
+      suffix : "no"
+    },
+    {
+      name : "prod",
+      env_type : "prod",
+      suffix : "no"
+    },
   ]
 
   namespaces_deployenvs = distinct(flatten([
@@ -26,12 +50,12 @@ locals {
 }
 
 resource "kubernetes_config_map_v1" "deploy_environments_manifests" {
-  for_each = { for entry in local.namespaces_deployenvs : "${entry.namespace}.${entry.deploy_env}" => entry }
+  for_each = { for entry in local.namespaces_deployenvs : "${entry.namespace}.${entry.deploy_env.name}" => entry }
   metadata {
-    name      = "deploy-environments-${each.value.deploy_env}"
+    name      = "deploy-environments-${each.value.deploy_env.name}"
     namespace = each.value.namespace
   }
   data = {
-    BASE_URL = each.value.deploy_env == "prod" ? "https://platform.altinn.no/" : "https://platform.${each.value.deploy_env}.altinn.no/"
+    BASE_URL = each.value.deploy_env.name == "prod" ? "https://platform.altinn.no" : "https://platform.${each.value.deploy_env.name}.altinn.${each.value.deploy_env.suffix}"
   }
 }
