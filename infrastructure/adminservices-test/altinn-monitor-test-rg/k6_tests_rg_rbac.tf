b3e45a49cefe2a0ad0de38dc70c76041170dff41
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-03-28 08:30:07 +0100

    chore: Add Azure Kubernetes Service Cluster User Role to AltinnPlatform-K6-Cluster-Users (#1485)

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_rbac.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_rbac.tf
@@ -1,3 +1,9 @@
+resource "azurerm_role_assignment" "azure_kubernetes_service_cluster_user_role" {
+  scope                = azurerm_kubernetes_cluster.k6tests.id
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
+  principal_id         = "b95b1fc9-7f21-49c3-8932-07161cd9ac5a"
+}
+
 resource "kubernetes_cluster_role_v1" "dev_access" {
   metadata {
     name = "dev-access"
