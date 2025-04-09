dbfc0fdb9bcaecac362b1f7d1d1ca939ca277ab4
tjololo <1145298+tjololo@users.noreply.github.com>
2025-04-09 13:20:23 +0200

    feat: add acrpull and reader IAMs

--- infrastructure/adminservices-prod/altinncr/iam.tf
+++ infrastructure/adminservices-prod/altinncr/iam.tf
@@ -5,3 +5,19 @@ resource "azurerm_role_assignment" "altinncr_acrpush" {
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
+
+resource "azurerm_role_assignment" "altinncr_acrpull" {
+  for_each                         = var.acr_pull_object_ids
+  principal_id                     = each.value
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "altinncr_reader" {
+  for_each                         = var.acr_pull_object_ids
+  principal_id                     = each.value
+  role_definition_name             = "Reader"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
