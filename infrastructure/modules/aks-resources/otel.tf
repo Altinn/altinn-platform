c075aafabe02f3a7438c5dfa39c9fff53d7552d8
Sebastian Duran <duran.c.sebastian@gmail.com>
2025-04-29 07:34:57 +0100

    feat(aks-resources): add otel (#1587)
    * feat(aks-resources): add otel
    
    * typo

---
+++ infrastructure/modules/aks-resources/otel.tf
@@ -0,0 +1,69 @@
+resource "azapi_resource" "otel_operator" {
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "otel-operator"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        otel-operator = {
+          force                  = false
+          path                   = "./"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = true
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/otel-operator"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = "flux-system"
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
+
+resource "azapi_resource" "otel_collector" {
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "otel-collector"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        otel-collector = {
+          force                  = false
+          path                   = "./"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = true
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/otel-collector"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = "flux-system"
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
