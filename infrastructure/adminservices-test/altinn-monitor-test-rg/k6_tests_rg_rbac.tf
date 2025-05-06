d5b803e35953de289d4cd81590abda1185013ebd
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-05-06 13:00:10 +0200

    feat: allow k6 users access to log analytics workspace

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_rbac.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_rbac.tf
@@ -4,6 +4,12 @@ resource "azurerm_role_assignment" "azure_kubernetes_service_cluster_user_role" 
   principal_id         = "b95b1fc9-7f21-49c3-8932-07161cd9ac5a"
 }
 
+resource "azurerm_role_assignment" "reader_user_role" {
+  scope                = azurerm_log_analytics_workspace.k6tests.id
+  role_definition_name = "Reader"
+  principal_id         = "b95b1fc9-7f21-49c3-8932-07161cd9ac5a"
+}
+
 resource "kubernetes_cluster_role_v1" "dev_access" {
   metadata {
     name = "dev-access"
