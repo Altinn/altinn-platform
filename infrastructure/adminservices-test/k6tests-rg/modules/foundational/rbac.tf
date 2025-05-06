9c8f3c6605f2b217cb8507e2421286a0d0e55996
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-05-06 13:20:04 +0200

    feat: allow k6 users access to log analytics workspace (#1604)

--- infrastructure/adminservices-test/k6tests-rg/modules/foundational/rbac.tf
+++ infrastructure/adminservices-test/k6tests-rg/modules/foundational/rbac.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "azure_kubernetes_service_cluster_user_role" 
   role_definition_name = "Azure Kubernetes Service Cluster User Role"
   principal_id         = var.k8s_users_group_object_id
 }
+
+resource "azurerm_role_assignment" "reader_user_role" {
+  scope                = azurerm_log_analytics_workspace.k6tests.id
+  role_definition_name = "Reader"
+  principal_id         = var.k8s_users_group_object_id
+}
