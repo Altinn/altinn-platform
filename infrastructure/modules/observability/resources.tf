3e5553cd0c719ae9e007b1cdb3af7c67779b3fbc
Sebastian Duran <duran.c.sebastian@gmail.com>
2025-05-07 19:07:22 +0200

    add suggestions and tf fmt

--- infrastructure/modules/observability/resources.tf
+++ infrastructure/modules/observability/resources.tf
@@ -1,15 +1,15 @@
 resource "azurerm_log_analytics_workspace" "obs" {
   name                = var.log_analytics_workspace_name != "" ? var.log_analytics_workspace_name : "${var.prefix}-${var.environment}-obs-law"
-  resource_group_name = var.azurerm_resource_group_obs_name
+  resource_group_name = azurerm_resource_group.obs.name
   location            = var.location
   retention_in_days   = var.log_analytics_retention_days
 
-  tags = var.tags   
+  tags = var.tags
 }
 
 resource "azurerm_monitor_workspace" "obs" {
   name                = var.monitor_workspace_name != "" ? var.monitor_workspace_name : "${var.prefix}-${var.environment}-obs-amw"
-  resource_group_name = var.azurerm_resource_group_obs_name
+  resource_group_name = azurerm_resource_group.obs.name
   location            = var.location
 
   tags = var.tags
@@ -17,12 +17,12 @@ resource "azurerm_monitor_workspace" "obs" {
 
 resource "azurerm_application_insights" "obs" {
 
-  name                 = var.app_insights_name != "" ? var.app_insights_name : "${var.prefix}-${var.environment}-obs-appinsights"
-  resource_group_name = var.azurerm_resource_group_obs_name
+  name                = var.app_insights_name != "" ? var.app_insights_name : "${var.prefix}-${var.environment}-obs-appinsights"
+  resource_group_name = azurerm_resource_group.obs.name
   location            = var.location
-  workspace_id         = azurerm_log_analytics_workspace.obs.id
-  application_type     = "web"
-  retention_in_days    = 30
+  workspace_id        = azurerm_log_analytics_workspace.obs.id
+  application_type    = var.app_insights_app_type
+  retention_in_days   = 30
 
   tags = var.tags
 }
