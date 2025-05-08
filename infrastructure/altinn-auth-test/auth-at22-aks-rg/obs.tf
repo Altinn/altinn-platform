83ad8e188e4a61456324f8ef8429f348ef6d1e17
Sebastian Duran <duran.c.sebastian@gmail.com>
2025-05-08 09:39:37 +0200

    tf fmt

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/obs.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/obs.tf
@@ -1,10 +1,10 @@
 module "observability" {
-    source = "../../modules/observability"
-    depends_on = [module.aks]
-    prefix = "auth"
-    environment = "at22"
-    location = var.location
-    azurerm_resource_group_obs_name       = module.aks.aks_node_resource_group
-    kube_context = module.aks.aks_name
-    kubeconfig_path = "~/.kube/config"
+  source                          = "../../modules/observability"
+  depends_on                      = [module.aks]
+  prefix                          = "auth"
+  environment                     = "at22"
+  location                        = var.location
+  azurerm_resource_group_obs_name = module.aks.aks_node_resource_group
+  kube_context                    = module.aks.aks_name
+  kubeconfig_path                 = "~/.kube/config"
 }
