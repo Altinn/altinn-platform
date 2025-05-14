212a3c4a233479c5e47f7d5034b7070242751d08
Dagfinn Olsen <dagfinno@gmail.com>
2025-05-14 15:10:01 +0200

    fix exports

--- libs/k6/src/index.js
+++ libs/k6/src/index.js
@@ -1,2 +1,2 @@
 export { postSlackMessage } from './slack/slack.js';
-export { getEnterpriseToken, getEnterpriseTokenWithType, getPersonalToken, getAmToken} from './token/token.js';
+export { getEnterpriseToken, getEnterpriseTokenWithType, getPersonalToken, getAmToken} from './common/token.js';
