49f7f63bd4240cc22867bd69744350ceb0db06e5
Dagfinn Olsen <dagfinno@gmail.com>
2025-05-14 15:15:18 +0200

    add describe

--- libs/k6/src/index.js
+++ libs/k6/src/index.js
@@ -1,2 +1,3 @@
 export { postSlackMessage } from './slack/slack.js';
 export { getEnterpriseToken, getEnterpriseTokenWithType, getPersonalToken, getAmToken} from './common/token.js';
+export { describe } from './common/describe.js';
