00529ee490bc3063853a3278c75b0056e089957d
Dagfinn Olsen <dagfinno@gmail.com>
2025-05-15 08:39:18 +0200

    fix tokengen

--- libs/k6/src/index.js
+++ libs/k6/src/index.js
@@ -1,5 +1,5 @@
 export { postSlackMessage } from './slack/slack.js';
 export { getEnterpriseToken, getPersonalToken } from './common/token.js';
 export { describe } from './common/describe.js';
-export { customConsole } from './common/customConsole.js';
+export { customConsole } from './common/console.js';
 export { chai, expect, expectStatusFor } from './common/k6chai.js';
