1ef78c98ba693e4917a6e163130334620d54a3d1
Sebastian Duran <duran.c.sebastian@gmail.com>
2025-05-15 15:19:59 +0200

    lint

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/obs.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/obs.tf
@@ -1,7 +1,7 @@
 module "observability" {
-  source                          = "../../modules/observability"
-  depends_on                      = [module.aks]
-  prefix                          = "auth"
-  environment                     = "at22"
-  oidc_issuer_url                 = module.aks.aks_oidc_issuer_url
+  source          = "../../modules/observability"
+  depends_on      = [module.aks]
+  prefix          = "auth"
+  environment     = "at22"
+  oidc_issuer_url = module.aks.aks_oidc_issuer_url
 }
