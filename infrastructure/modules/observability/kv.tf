32f916c41fcbf2d2b7019143f32470317c6126b7
tjololo <1145298+tjololo@users.noreply.github.com>
2025-05-16 14:08:10 +0200

    principal_id should be object_id not id

--- infrastructure/modules/observability/kv.tf
+++ infrastructure/modules/observability/kv.tf
@@ -17,18 +17,18 @@ resource "azurerm_key_vault" "obs_kv" {
 resource "azurerm_role_assignment" "obs_kv_reader" {
   scope                            = azurerm_key_vault.obs_kv.id
   role_definition_name             = "Key Vault Secrets User"
-  principal_id                     = azuread_service_principal.sp.id
+  principal_id                     = azuread_service_principal.sp.object_id
   skip_service_principal_aad_check = true
 }
 
 ## add connection string to key vault
 resource "azurerm_key_vault_secret" "conn_string" {
-  name         = "connectionString"
-  value        = azurerm_application_insights.obs.connection_string
-  key_vault_id = azurerm_key_vault.obs_kv.id
+  name            = "connectionString"
+  value           = azurerm_application_insights.obs.connection_string
+  key_vault_id    = azurerm_key_vault.obs_kv.id
   expiration_date = timeadd(timestamp(), "8760h") # 1 year
 
   lifecycle {
-    ignore_changes = [expiration_date]   # stop perpetual updates
+    ignore_changes = [expiration_date] # stop perpetual updates
   }
 }
