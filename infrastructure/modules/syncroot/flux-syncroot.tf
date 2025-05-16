a5e09570282b775e0c9e20b7e9f2aec052a4079c
Vemund Gaukstad <tjololo@users.noreply.github.com>
2025-05-16 11:31:34 +0200

    feat: flux syncroot module (#1625)
    * feat: flux syncroot module
    create module for deploying flux resources for sync root.
    resources are deployed to a team specific namespace
    
    * add optional namespace variable and fix comments from coderabbit
    
    * Update infrastructure/modules/syncroot/flux-syncroot.tf
    
    ---------
    
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

---
+++ infrastructure/modules/syncroot/flux-syncroot.tf
@@ -0,0 +1,34 @@
+resource "azapi_resource" "flux_syncroot" {
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "syncroot-${var.team_name}-${var.environment}"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        syncroot = {
+          force = false
+          path  = "./${var.environment}"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 600
+          wait                   = true
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.environment
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/${var.team_name}/syncroot"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = var.namespace != "" ? var.namespace : var.team_name
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
