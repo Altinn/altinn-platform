e813e74e482579805da978599f2a16af754c1661
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-20 14:17:24 +0200

    add obs amw

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/grafana.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/grafana.tf
@@ -5,6 +5,7 @@ module "grafana" {
   tenant_id                       = local.tenant_id
   client_config_current_object_id = data.azurerm_client_config.current.object_id
   workspace_integrations = [
-    module.aks.azurerm_monitor_workspace_id
+    module.aks.azurerm_monitor_workspace_id,
+    module.observability.monitor_workspace_id
   ]
 }
