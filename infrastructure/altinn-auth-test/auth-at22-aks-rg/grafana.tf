ecf638dae91bd087b632241e0c9a924ab3d301f3
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-20 13:48:07 +0200

    update var

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/grafana.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/grafana.tf
@@ -4,5 +4,7 @@ module "grafana" {
   environment                     = "at22"
   tenant_id                       = local.tenant_id
   client_config_current_object_id = data.azurerm_client_config.current.object_id
-  workspace_integrations          = module.aks.azurerm_monitor_workspace_id
+  workspace_integrations = [
+    module.aks.azurerm_monitor_workspace_id
+  ]
 }
