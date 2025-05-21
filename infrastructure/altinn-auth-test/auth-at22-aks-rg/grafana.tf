fa75d2769d6e86c0c344c120fa71950f9e9cc96f
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-21 12:02:50 +0200

    add example ids for permissions @coderabbitai full review

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/grafana.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/grafana.tf
@@ -7,7 +7,13 @@ module "grafana" {
   monitor_workspace_id = [
     module.observability.monitor_workspace_id
   ]
-  grafana_admin_access                   = []
-  grafana_editor_access                  = []
-  grafana_monitor_reader_subscription_id = []
+  grafana_admin_access = [
+    "143ed28a-6e6d-4ca0-8273-eecb9c1665ba", # Altinn-30-Test-Operations
+  ]
+  grafana_editor_access = [
+    "416302ed-fbab-41a4-8c8d-61f486fa79ca", # Altinn-30-Test-developers
+  ]
+  grafana_monitor_reader_subscription_id = [
+    var.subscription_id,
+  ]
 }
