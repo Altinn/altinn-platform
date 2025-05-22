2da0ca73916e8079d93dbb0c598d57a76d79a39d
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-22 15:19:39 +0200

    Remove kubectl provider dependency

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/providers.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/providers.tf
@@ -16,10 +16,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.0"
-    }
     http = {
       source  = "hashicorp/http"
       version = "~> 3.0"
