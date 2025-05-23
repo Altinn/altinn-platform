23933aea18f949951f2eed9819d4186796aed87c
tjololo <1145298+tjololo@users.noreply.github.com>
2025-05-23 13:59:53 +0200

    fix small code smells

--- infrastructure/adminservices-prod/altinncr/managed-identities.tf
+++ infrastructure/adminservices-prod/altinncr/managed-identities.tf
@@ -1,10 +1,10 @@
-resource "azurerm_user_assigned_identity" "github-pusher" {
+resource "azurerm_user_assigned_identity" "github_pusher" {
   name                = "github-altinn-platform-pusher"
   location            = azurerm_resource_group.acr.location
   resource_group_name = azurerm_resource_group.acr.name
 }
 
-resource "azurerm_federated_identity_credential" "altinn-platform-federation-main" {
+resource "azurerm_federated_identity_credential" "altinn_platform_federation_main" {
   name                = "github.altinn.altinn-platform.ref.main"
   parent_id           = azurerm_user_assigned_identity.github-pusher.id
   resource_group_name = azurerm_resource_group.acr.name
@@ -13,7 +13,7 @@ resource "azurerm_federated_identity_credential" "altinn-platform-federation-mai
   subject             = "repo:Altinn/altinn-platform:ref:refs/heads/main"
 }
 
-resource "azurerm_federated_identity_credential" "altinn-platform-federation-flux-release" {
+resource "azurerm_federated_identity_credential" "altinn_platform_federation_flux_release" {
   name                = "github.altinn.altinn-platform.environment.flux-release"
   parent_id           = azurerm_user_assigned_identity.github-pusher.id
   resource_group_name = azurerm_resource_group.acr.name
