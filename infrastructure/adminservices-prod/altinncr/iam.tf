1f1d7e535171cba09a279d58463dd62d3cda7dc0
tjololo <1145298+tjololo@users.noreply.github.com>
2025-05-23 13:50:23 +0200

    add user assigned identity for acrpush from altinn-platform

--- infrastructure/adminservices-prod/altinncr/iam.tf
+++ infrastructure/adminservices-prod/altinncr/iam.tf
@@ -24,3 +24,11 @@ resource "azurerm_role_assignment" "altinncr_reader" {
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
+
+resource "azurerm_role_assignment" "altinncr_acrpush_altinn_platform" {
+  principal_id                     = azurerm_user_assigned_identity.github-pusher.principal_id
+  role_definition_name             = "AcrPush"
+  principal_type                   = "ServicePrincipal"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
---
+++ infrastructure/adminservices-prod/altinncr/managed-identities.tf
@@ -0,0 +1,23 @@
+resource "azurerm_user_assigned_identity" "github-pusher" {
+  name                = "github-altinn-platform-pusher"
+  location            = azurerm_resource_group.acr.location
+  resource_group_name = azurerm_resource_group.acr.name
+}
+
+resource "azurerm_federated_identity_credential" "altinn-platform-federation-main" {
+  name                = "github.altinn.altinn-platform.ref.main"
+  parent_id           = azurerm_user_assigned_identity.github-pusher.id
+  resource_group_name = azurerm_resource_group.acr.name
+  audience            = "api://AzureADTokenExchange"
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:Altinn/altinn-platform:ref:refs/heads/main"
+}
+
+resource "azurerm_federated_identity_credential" "altinn-platform-federation-flux-release" {
+  name                = "github.altinn.altinn-platform.environment.flux-release"
+  parent_id           = azurerm_user_assigned_identity.github-pusher.id
+  resource_group_name = azurerm_resource_group.acr.name
+  audience            = "api://AzureADTokenExchange"
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:Altinn/altinn-platform:environment:flux-release"
+}
