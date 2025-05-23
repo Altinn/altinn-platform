4fc709908a15d5ccfb281cec877e8fc70fe1190d
Vemund Gaukstad <tjololo@users.noreply.github.com>
2025-05-23 14:18:24 +0200

    feat: add user assigned identity for acrpush from altinn-platform (#1673)
    * add user assigned identity for acrpush from altinn-platform
    
    * fix small code smells
    
    * fix references after refactor
    
    * fix audience
    
    ---------
    
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

---
+++ infrastructure/adminservices-prod/altinncr/managed-identities.tf
@@ -0,0 +1,23 @@
+resource "azurerm_user_assigned_identity" "github_pusher" {
+  name                = "github-altinn-platform-pusher"
+  location            = azurerm_resource_group.acr.location
+  resource_group_name = azurerm_resource_group.acr.name
+}
+
+resource "azurerm_federated_identity_credential" "altinn_platform_federation_main" {
+  name                = "github.altinn.altinn-platform.ref.main"
+  parent_id           = azurerm_user_assigned_identity.github_pusher.id
+  resource_group_name = azurerm_resource_group.acr.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:Altinn/altinn-platform:ref:refs/heads/main"
+}
+
+resource "azurerm_federated_identity_credential" "altinn_platform_federation_flux_release" {
+  name                = "github.altinn.altinn-platform.environment.flux-release"
+  parent_id           = azurerm_user_assigned_identity.github_pusher.id
+  resource_group_name = azurerm_resource_group.acr.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:Altinn/altinn-platform:environment:flux-release"
+}
