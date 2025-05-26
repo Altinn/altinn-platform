ebd71bf3a0401cc4b63c6fa638984b60d9b1ee33
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-26 12:01:17 +0200

    Update obs.tf

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/obs.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/obs.tf
@@ -1,7 +1,8 @@
 module "observability" {
-  source          = "../../modules/observability"
-  depends_on      = [module.aks]
-  prefix          = "auth"
-  environment     = "at22"
-  oidc_issuer_url = module.aks.aks_oidc_issuer_url
+  source                        = "../../modules/observability"
+  depends_on                    = [module.aks]
+  prefix                        = "auth"
+  environment                   = "at22"
+  azurerm_kubernetes_cluster_id = module.aks.azurerm_kubernetes_cluster_id
+  oidc_issuer_url               = module.aks.aks_oidc_issuer_url
 }
