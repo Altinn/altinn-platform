244455f0332a33360df8a354cd722ae892e60ede
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-26 11:56:26 +0200

    Simplify Kubernetes cluster variable reference
    Change from object notation to direct string variable for cluster ID
    to streamline Terraform configuration

--- infrastructure/modules/observability/amw-collection.tf
+++ infrastructure/modules/observability/amw-collection.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_data_collection_endpoint" "amw" {
-  count               = var.azurerm_kubernetes_cluster.aks.id != "" ? 1 : 0
+  count               = var.azurerm_kubernetes_cluster_id != "" ? 1 : 0
   name                = "${azurerm_monitor_workspace.obs.name}-mdce"
   resource_group_name = azurerm_resource_group.obs.name
   location            = azurerm_resource_group.obs.location
@@ -7,7 +7,7 @@ resource "azurerm_monitor_data_collection_endpoint" "amw" {
 }
 
 resource "azurerm_monitor_data_collection_rule" "amw" {
-  count                       = var.azurerm_kubernetes_cluster.aks.id != "" ? 1 : 0
+  count                       = var.azurerm_kubernetes_cluster_id != "" ? 1 : 0
   name                        = "${azurerm_monitor_workspace.obs.name}-mdcr"
   resource_group_name         = azurerm_resource_group.obs.name
   location                    = azurerm_resource_group.obs.location
@@ -41,9 +41,9 @@ resource "azurerm_monitor_data_collection_rule" "amw" {
 }
 
 resource "azurerm_monitor_data_collection_rule_association" "amw" {
-  count                   = var.azurerm_kubernetes_cluster.aks.id != "" ? 1 : 0
+  count                   = var.azurerm_kubernetes_cluster_id != "" ? 1 : 0
   name                    = "${azurerm_monitor_workspace.obs.name}-mdcra"
-  target_resource_id      = var.azurerm_kubernetes_cluster.aks.id
+  target_resource_id      = var.azurerm_kubernetes_cluster_id
   data_collection_rule_id = azurerm_monitor_data_collection_rule.amw[0].id
   description             = "Association of data collection rule. Deleting this association will break the data collection for this AKS Cluster."
   depends_on = [
