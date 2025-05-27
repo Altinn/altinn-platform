3e803495f53157b2c930ca578f8f7671d640d210
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-05-27 13:42:01 +0200

    Add Grafana manifests Flux configuration to AKS

---
+++ infrastructure/modules/aks-resources/grafana-manifests.tf
@@ -0,0 +1,35 @@
+resource "azapi_resource" "grafana_manifests" {
+  depends_on = [azapi_resource.linkerd]
+  type       = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name       = "grafana-manifests"
+  parent_id  = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        grafana-manifests = {
+          force                  = false
+          path                   = "./dashboards/"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = true
+        }
+      }
+      namespace = "flux-system"
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/grafana-manifests"
+        useWorkloadIdentity   = true
+      }
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
