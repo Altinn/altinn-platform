9d2d44a238e9705851f630994247a06663522a98
Bengt <32734566+bengtfredh@users.noreply.github.com>
2025-06-12 12:53:55 +0200

    fix: admin-test-aks appy - update provider kubectl (#1807)
    * update provider kubectl
    
    * Update providers.tf
    
    * Update providers.tf
    
    * Update providers.tf
    
    ---------
    
    Co-authored-by: Bengt Fredh <bengt.rino.fredh@digdir.no>

--- infrastructure/adminservices-test/admin-test-aks-rg/providers.tf
+++ infrastructure/adminservices-test/admin-test-aks-rg/providers.tf
@@ -31,15 +31,15 @@ provider "azurerm" {
     "Microsoft.Dashboard",
     "Microsoft.KubernetesConfiguration"
   ]
+  storage_use_azuread = true
 }
 
 provider "kubectl" {
-  load_config_file = false
-  # Use kube_config instead of kube_admin_config when Azure RBAC is enabled
-  client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
-  client_key             = base64decode(module.aks.kube_config.0.client_key)
-  host                   = module.aks.kube_config.0.host
-  cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
+  load_config_file       = false
+  host                   = module.aks.kube_admin_config[0].host
+  client_certificate     = base64decode(module.aks.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(module.aks.kube_admin_config[0].client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_admin_config[0].cluster_ca_certificate)
 }
 
 provider "azapi" {
