5779bf99b20c18bb7686b6dbb3b76786f865e329
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-06-12 12:03:04 +0200

    Update providers.tf

--- infrastructure/adminservices-test/admin-test-aks-rg/providers.tf
+++ infrastructure/adminservices-test/admin-test-aks-rg/providers.tf
@@ -35,10 +35,10 @@ provider "azurerm" {
 
 provider "kubectl" {
   load_config_file       = false
-  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
+  host                   = module.aks.kube_admin_config[0].host
+  client_certificate     = base64decode(module.aks.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(module.aks.kube_admin_config[0].client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_admin_config[0].cluster_ca_certificate)
 }
 
 provider "azapi" {
