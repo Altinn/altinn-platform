f5c0eba49f14b52f81dfbcddcdab9d3493c6af47
tjololo <1145298+tjololo@users.noreply.github.com>
2025-06-13 10:09:54 +0200

    add missing variable

--- infrastructure/altinn-auth-test/auth-at22-aks-rg/azure-service-operator.tf
+++ infrastructure/altinn-auth-test/auth-at22-aks-rg/azure-service-operator.tf
@@ -5,4 +5,5 @@ module "azure_service_operator" {
   azurerm_kubernetes_cluster_oidc_issuer_url = module.aks.aks_oidc_issuer_url
   azurerm_kubernetes_cluster_id              = module.aks.azurerm_kubernetes_cluster_id
   azurerm_subscription_id                    = var.subscription_id
+  dis_resource_group_id                      = module.aks.dis_resource_group_id
 }
