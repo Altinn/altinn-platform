609bd27f7fa1c3317921ac45dc2e72a4038ed349
tjololo <1145298+tjololo@users.noreply.github.com>
2025-06-13 09:39:33 +0200

    change from contributor to custom role

--- infrastructure/modules/azure-service-operator/identity.tf
+++ infrastructure/modules/azure-service-operator/identity.tf
@@ -20,8 +20,33 @@ resource "azurerm_federated_identity_credential" "aso_fic" {
   parent_id           = azurerm_user_assigned_identity.aso_identity.id
 }
 
+resource "azurerm_role_definition" "user_assigned_identity_role" {
+  name        = "DIS User Assigned Identity Admin Role"
+  scope       = var.dis_resource_group_id
+  description = "Role for Dis deployed Azure Service Operator to manage resources in the specified resource group."
+
+  permissions {
+    actions = [
+      "Microsoft.ManagedIdentity/userAssignedIdentities/read",
+			"Microsoft.ManagedIdentity/userAssignedIdentities/write",
+			"Microsoft.ManagedIdentity/userAssignedIdentities/delete",
+			"Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials/read",
+			"Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials/write",
+			"Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials/delete",
+			"Microsoft.ManagedIdentity/userAssignedIdentities/revokeTokens/action",
+			"Microsoft.Authorization/*/read",
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    var.dis_resource_group_id
+  ]
+  
+}
+
 resource "azurerm_role_assignment" "aso_contrib_role_assignment" {
   scope                = var.dis_resource_group_id
-  role_definition_name = "Contributor"
+  role_definition_name = azurerm_role_definition.user_assigned_identity_role.name
   principal_id         = azurerm_user_assigned_identity.aso_identity.id
 }
