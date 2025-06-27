a813e4ebc0bd990302a56d069cbacb245da8a360
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-06-27 09:13:23 +0200

    maskinporten

--- libs/k6/src/index.js
+++ libs/k6/src/index.js
@@ -1,2 +1,3 @@
 //export { postSlackMessage } from './slack/slack.js';
 export { TokenGenerator } from "./token_generator/token_generator.js"
+export { MaskinportenAccessTokenGenerator } from "./maskinporten/maskinporten.js"
