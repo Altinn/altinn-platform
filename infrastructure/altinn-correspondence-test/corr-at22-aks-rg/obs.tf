41168f1e9b6a12f8c6cc161b0b0ef296a8b632d3
tjololo <1145298+tjololo@users.noreply.github.com>
2025-08-20 10:33:05 +0200

    feat(dis-cr-corr): set correct env and add newly added variable in obs module

--- infrastructure/altinn-correspondence-test/corr-at22-aks-rg/obs.tf
+++ infrastructure/altinn-correspondence-test/corr-at22-aks-rg/obs.tf
@@ -3,6 +3,7 @@ module "observability" {
   depends_on                    = [module.aks]
   prefix                        = var.team_name
   environment                   = var.environment
+  enable_aks_monitoring         = true
   azurerm_kubernetes_cluster_id = module.aks.azurerm_kubernetes_cluster_id
   oidc_issuer_url               = module.aks.aks_oidc_issuer_url
 }
