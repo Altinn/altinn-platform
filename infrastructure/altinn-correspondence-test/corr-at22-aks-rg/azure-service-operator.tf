cfba38a3a62d8b9f5dc9925391bab67e4de12c77
Vemund Gaukstad <tjololo@users.noreply.github.com>
2025-08-20 15:16:01 +0200

    feat(dis-cr-corr): setup iac and workflow for deployment of correspondence cluster (#2087)
    * feat(dis-cr-corr): setup iac and workflow for deployment of correspondence cluster
    
    copy past of auth, with some modifications
    * make aks vnet address space a variable
    * make team name a variable
    * make environment a variable
    * make flux release tag a variabel
    * ensure usage of variables in the terraform code
    
    * feat(dis-cr-corr): set correct env and add newly added variable in obs module
    
    * fix(dis-cr-corr): update script to match new module vars
    
    ---------
    
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

---
+++ infrastructure/altinn-correspondence-test/corr-at22-aks-rg/azure-service-operator.tf
@@ -0,0 +1,10 @@
+module "azure_service_operator" {
+  source                                     = "../../modules/azure-service-operator"
+  prefix                                     = var.team_name
+  environment                                = var.environment
+  azurerm_kubernetes_cluster_oidc_issuer_url = module.aks.aks_oidc_issuer_url
+  azurerm_kubernetes_cluster_id              = module.aks.azurerm_kubernetes_cluster_id
+  azurerm_subscription_id                    = var.subscription_id
+  dis_resource_group_id                      = module.aks.dis_resource_group_id
+  flux_release_tag                           = var.flux_release_tag
+}
