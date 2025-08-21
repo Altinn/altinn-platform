415b4f09ae143c3d9777fed89ddcfa5edd66d5bc
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-08-21 13:54:53 +0200

    chore: use the more readable grafana domain

--- libs/k6/src/config.js
+++ libs/k6/src/config.js
@@ -1,7 +1,7 @@
 export const config = {
   grafanaBaseUrl:
     __ENV.GRAFANA_BASE_URL ??
-    'https://altinn-grafana-test-b2b8dpdkcvfuhfd3.eno.grafana.azure.com',
+    'https://grafana.altinn.cloud',
   k6PrometheusDashboard:
     __ENV.K6_PROMETHEUS_DASHBOARD ??
     'd/ccbb2351-2ae2-462f-ae0e-f2c893ad1028/k6-prometheus',
