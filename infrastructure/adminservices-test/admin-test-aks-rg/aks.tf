ce22075ead5e703f8290e62433b721366b93bfa8
tjololo <1145298+tjololo@users.noreply.github.com>
2025-09-19 12:05:55 +0200

    fix(admin-aks): new variable not added to adminservices test cluster

--- infrastructure/adminservices-test/admin-test-aks-rg/aks.tf
+++ infrastructure/adminservices-test/admin-test-aks-rg/aks.tf
@@ -50,4 +50,5 @@ module "aks_resources" {
   token_grafana_operator           = var.token_grafana_operator
   grafana_dashboard_release_branch = "main"
   enable_grafana_operator          = true
+  subscription_id                  = var.subscription_id
 }
