be519201577ab3b742ef651a52c02870788c49b7
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-10-14 10:52:02 +0200

    feat: add TokenGenerator module to k6 lib (#1926)

--- libs/k6/src/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  grafanaBaseUrl:
-    __ENV.GRAFANA_BASE_URL ??
-    'https://grafana.altinn.cloud',
-  k6PrometheusDashboard:
-    __ENV.K6_PROMETHEUS_DASHBOARD ??
-    'd/ccbb2351-2ae2-462f-ae0e-f2c893ad1028/k6-prometheus',
-  datasource: __ENV.DATASOURCE ?? 'k6tests-amw',
-};
