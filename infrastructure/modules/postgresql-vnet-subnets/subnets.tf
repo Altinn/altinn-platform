e8c1ac6368b12201a2d159d8a3d8d64afe5c696c
Vemund Gaukstad <tjololo@users.noreply.github.com>
2025-12-09 10:39:39 +0100

    chroe(admin-test): deploy pgsql vnet for testpurposes (#2788)
    * chroe(admin-test): deploy pgsql vnet for testpurposes
    
    * fix formating
    
    * fix variables
    
    * fix subnet_indices
    
    * fix for_each
    
    * fix ip cidr regex
    
    * simplify and improve condition for ip cidr
    
    ---------
    
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

--- infrastructure/modules/postgresql-vnet-subnets/subnets.tf
+++ infrastructure/modules/postgresql-vnet-subnets/subnets.tf
@@ -1,9 +1,9 @@
 locals {
-  subnet_indices = range(16) # Creates a list of numbers from 0 to 15
+  subnet_indices = toset(range(16)) # Creates a set of numbers from 0 to 15
 }
 
 resource "azurerm_subnet" "postgresql_subnets" {
-  for_each                          = toset(local.subnet_indices)
+  for_each                          = toset([for i in local.subnet_indices : tostring(i)])
   address_prefixes                  = [cidrsubnet(var.vnet_address_space, 4, each.value)]
   name                              = "${var.name}-subnet-${each.value}"
   resource_group_name               = var.resource_group_name
