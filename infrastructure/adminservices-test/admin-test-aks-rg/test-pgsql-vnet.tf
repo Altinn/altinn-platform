b0a62a845b333a93ef84971c0a2e3e9a2945e705
tjololo <1145298+tjololo@users.noreply.github.com>
2025-12-09 09:07:53 +0100

    fix variables

--- infrastructure/adminservices-test/admin-test-aks-rg/test-pgsql-vnet.tf
+++ infrastructure/adminservices-test/admin-test-aks-rg/test-pgsql-vnet.tf
@@ -4,10 +4,10 @@ module "test-pgsql-vnet" {
   resource_group_name = module.aks.dis_resource_group_name
   location            = "norwayeast"
   vnet_address_space  = var.pgsql_vnet_address_space
-  peered_vnets = [{
+  peered_vnets = {
     id                  = module.aks.aks_workpool_vnet_id
     name                = module.aks.aks_workpool_vnet_name
     resource_group_name = module.aks.aks_workpool_vnet_resource_group_name
-  }]
+  }
   tags = {}
 }
