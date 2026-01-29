04aeb0e017b0072d4088d49adaaba962e10b1a00
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-01-29 11:38:25 +0100

    fix: Add Terraform import blocks for Altinncr ACR roles
    Signed-off-by: Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>

---
+++ infrastructure/adminservices-prod/altinncr/import.tf
@@ -0,0 +1,14 @@
+import {
+  to = azurerm_role_assignment.altinncr_user_access_admin_acr_pull["4da564b5-c526-42f2-aa01-108c5b4932f2"]
+  id = "/subscriptions/cd3dc8fe-84eb-4e78-bd5c-81a3bc864f19/resourceGroups/acr/providers/Microsoft.ContainerRegistry/registries/altinncr|/subscriptions/cd3dc8fe-84eb-4e78-bd5c-81a3bc864f19/providers/Microsoft.Authorization/roleDefinitions/18d7d88d-d35e-4fb5-a5c3-7773c20a72d9|4da564b5-c526-42f2-aa01-108c5b4932f2"
+}
+
+import {
+  to = azurerm_role_assignment.altinncr_user_access_admin_acr_pull["9bfd0ad4-9f59-4ad5-b8e5-a9664ec375fd"]
+  id = "/subscriptions/cd3dc8fe-84eb-4e78-bd5c-81a3bc864f19/resourceGroups/acr/providers/Microsoft.ContainerRegistry/registries/altinncr|/subscriptions/cd3dc8fe-84eb-4e78-bd5c-81a3bc864f19/providers/Microsoft.Authorization/roleDefinitions/18d7d88d-d35e-4fb5-a5c3-7773c20a72d9|9bfd0ad4-9f59-4ad5-b8e5-a9664ec375fd"
+}
+
+import {
+  to = azurerm_role_assignment.altinncr_user_access_admin_acr_pull["476e7604-4701-48da-953e-e6616cfedd15"]
+  id = "/subscriptions/cd3dc8fe-84eb-4e78-bd5c-81a3bc864f19/resourceGroups/acr/providers/Microsoft.ContainerRegistry/registries/altinncr|/subscriptions/cd3dc8fe-84eb-4e78-bd5c-81a3bc864f19/providers/Microsoft.Authorization/roleDefinitions/18d7d88d-d35e-4fb5-a5c3-7773c20a72d9|476e7604-4701-48da-953e-e6616cfedd15"
+}
