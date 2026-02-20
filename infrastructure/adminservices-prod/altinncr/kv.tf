35d866824fd64b54aedd3876b28fa0c004c2263d
Vemund Gaukstad <tjololo@users.noreply.github.com>
2026-02-20 12:07:53 +0100

    chore: remove moved files from repo (#3125)
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

--- infrastructure/adminservices-prod/altinncr/kv.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_key_vault" "kv" {
-  name                = var.acrname
-  location            = azurerm_resource_group.acr.location
-  resource_group_name = azurerm_resource_group.acr.name
-  sku_name            = "standard"
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-}
