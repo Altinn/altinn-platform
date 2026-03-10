48c570b73afdf0508b449872e7c8d6dbef693230
tjololo <1145298+tjololo@users.noreply.github.com>
2026-03-10 12:21:47 +0100

    fix: support repos with . in the name

--- infrastructure/modules/syncroot-github-repo/azure-identity.tf
+++ infrastructure/modules/syncroot-github-repo/azure-identity.tf
@@ -1,5 +1,5 @@
 resource "azurerm_user_assigned_identity" "syncroot_pusher" {
-  name                = "${var.github_org_name}-${var.github_repo_name}-${var.product_name}-syncroot"
+  name                = "${var.github_org_name}-${replace(var.github_repo_name, ".", "_")}-${var.product_name}-syncroot"
   location            = var.location
   resource_group_name = var.resource_group_name
   tags = merge(var.tags, {
