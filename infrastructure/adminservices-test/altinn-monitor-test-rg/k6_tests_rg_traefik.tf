a821c5d490bc6190ad8dc7c9b049a3052c38502c
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-04-14 10:56:21 +0200

    chore: migrate from nginx to traefik

---
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_traefik.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "traefik" {
+  depends_on = [
+    azurerm_kubernetes_cluster.k6tests
+  ]
+  lint             = true
+  name             = "traefik"
+  namespace        = "traefik"
+  create_namespace = true
+  repository       = "https://traefik.github.io/charts"
+  chart            = "traefik"
+  version          = "39.0.7"
+  values = [
+    "${templatefile("${path.module}/k6_tests_rg_traefik_values.tftpl", {})}"
+  ]
+}
