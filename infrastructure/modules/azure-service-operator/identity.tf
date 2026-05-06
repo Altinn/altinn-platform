ed20c7a2993398911655f9a4b0cd2bfa5591b151
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-05-06 13:23:10 +0200

    chore: Remove unnecessary resource_group_name attribute from federated identity credential
    Remove unnecessary resource_group_name attribute from
    federated identity credential, which is now derived from
    parent_id in azurerm >= 4.60.0.

--- infrastructure/modules/azure-service-operator/identity.tf
+++ infrastructure/modules/azure-service-operator/identity.tf
@@ -12,9 +12,8 @@ resource "azurerm_user_assigned_identity" "aso_identity" {
 }
 
 resource "azurerm_federated_identity_credential" "aso_fic" {
-  name                = "aso-aks-${var.prefix}-${var.environment}"
-  resource_group_name = azurerm_resource_group.aso_rg.name
-  audience            = ["api://AzureADTokenExchange"]
+  name     = "aso-aks-${var.prefix}-${var.environment}"
+  audience = ["api://AzureADTokenExchange"]
   issuer              = var.azurerm_kubernetes_cluster_oidc_issuer_url
   subject             = "system:serviceaccount:${var.aso_namespace}:${var.aso_service_account_name}"
   parent_id           = azurerm_user_assigned_identity.aso_identity.id
