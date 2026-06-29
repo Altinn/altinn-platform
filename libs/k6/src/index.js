b1a1ea5d4d216b71e1a0c88d2dc8fbf1d4adfb8a
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-06-29 10:50:38 +0200

    Remove K6 lib, moved to altinn-platform-validation-tests repo (#3767)

--- libs/k6/src/index.js
+++
@@ -1,9 +0,0 @@
-export {
-  PersonalTokenGenerator,
-  PersonalTokenGeneratorOptions,
-  EnterpriseTokenGenerator,
-  EnterpriseTokenGeneratorOptions,
-  PlatformTokenGenerator,
-  PlatformTokenGeneratorOptions,
-} from './token_generator/token_generator.js';
-export { MaskinportenAccessTokenGenerator } from './maskinporten/maskinporten.js';
--- libs/k6/src/maskinporten/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  maskinportenBaseUrl: 'https://test.maskinporten.no',
-  audienceUrl: 'https://test.maskinporten.no/',
-  tokenUrl: 'https://test.maskinporten.no/token',
-};
-
-export { config };
