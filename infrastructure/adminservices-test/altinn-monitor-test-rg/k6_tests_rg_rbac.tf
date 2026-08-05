0d4affd79564c50dce187eb0e4937a8312f4bc47
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-08-05 18:15:39 +0200

    Add missing namespace for the different API (#3877)

--- infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_rbac.tf
+++ infrastructure/adminservices-test/altinn-monitor-test-rg/k6_tests_rg_rbac.tf
@@ -153,8 +153,33 @@ variable "k8s_rbac" {
       dev_group = "4dde4651-a9ca-4df1-9e05-216272284c7d"
       sp_group  = "e87d6f10-6fc0-4a09-a9b0-e8c994ed4052"
     }
+    events = {
+      namespace = "events"
+      dev_group = "4dde4651-a9ca-4df1-9e05-216272284c7d"
+      sp_group  = "e87d6f10-6fc0-4a09-a9b0-e8c994ed4052"
+    }
+    notifications = {
+      namespace = "notifications"
+      dev_group = "4dde4651-a9ca-4df1-9e05-216272284c7d"
+      sp_group  = "e87d6f10-6fc0-4a09-a9b0-e8c994ed4052"
+    }
+    profil = {
+      namespace = "profil"
+      dev_group = "4dde4651-a9ca-4df1-9e05-216272284c7d"
+      sp_group  = "e87d6f10-6fc0-4a09-a9b0-e8c994ed4052"
+    }
+    storage = {
+      namespace = "storage"
+      dev_group = "4dde4651-a9ca-4df1-9e05-216272284c7d"
+      sp_group  = "e87d6f10-6fc0-4a09-a9b0-e8c994ed4052"
+    }
     authentication = {
       namespace = "authentication"
+      dev_group = "5c42ac79-86e2-46d0-85d3-ae751dd5f057"
+      sp_group  = "328cbe61-aeb1-4782-bb36-d288c69b4f15"
+    },
+    "resource-registry" = {
+      namespace = "resource-registry"
       dev_group = "5c42ac79-86e2-46d0-85d3-ae751dd5f057"
       sp_group  = "328cbe61-aeb1-4782-bb36-d288c69b4f15"
     },
