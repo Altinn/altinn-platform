24111f6767530696477b1e533e84b8c18803b9a7
tjololo <1145298+tjololo@users.noreply.github.com>
2026-08-19 11:14:06 +0200

    chore: deploy ghrunners for altinn support test

---
+++ infrastructure/gh-runners/altinn-support-test-private.tf
@@ -0,0 +1,18 @@
+# GitHub: Altinn/altinn-support-test-private (product: altinn-support)
+module "gh_runners_support_test" {
+  source = "../modules/gh-runners"
+
+  resource_group_name           = azurerm_resource_group.gh_runners.name
+  repository_name               = "altinn-support-test-private"
+  private_runners_address_space = "172.17.136.0/24"
+  private_runners_prefix        = "as-test"
+  altinn_app_id                 = var.altinn_app_id
+  altinn_app_install_id         = var.altinn_app_install_id
+  altinn_app_key                = var.altinn_app_key
+  host_ip                       = var.host_ip
+  runner_labels                 = "altinn-support-test-runner"
+  tags = merge(local.tags, {
+    finops_product = "altinn-support"
+    product        = "altinn-support"
+  })
+}
