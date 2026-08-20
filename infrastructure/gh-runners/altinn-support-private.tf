a5d9011e4d97f56c82d17d8e46b422f744ddeba0
Vemund Gaukstad <tjololo@users.noreply.github.com>
2026-08-20 13:12:45 +0200

    chore: custom image for altinn-support (#3916)
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

--- infrastructure/gh-runners/altinn-support-private.tf
+++ infrastructure/gh-runners/altinn-support-private.tf
@@ -11,6 +11,7 @@ module "gh_runners_support" {
   altinn_app_key                = var.altinn_app_key
   host_ip                       = var.host_ip
   runner_labels                 = "altinn-support-runner"
+  runner_image                  = "ghcr.io/altinn/altinn-support-private/gh-runner-image:latest"
   tags = merge(local.tags, {
     finops_product = "altinn-support"
     product        = "altinn-support"
