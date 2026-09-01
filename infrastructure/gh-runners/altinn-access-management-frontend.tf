6dca24b9c9cc7058763ae50c8788cbb02aa4e119
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-09-01 16:17:00 +0200

    Use a custom image in the am frontend runners

--- infrastructure/gh-runners/altinn-access-management-frontend.tf
+++ infrastructure/gh-runners/altinn-access-management-frontend.tf
@@ -9,7 +9,7 @@ module "gh_runners_altinn-access-management-frontend" {
   altinn_app_install_id         = var.altinn_app_install_id
   altinn_app_key                = var.altinn_app_key
   host_ip                       = var.host_ip
-  runner_image                  = "ghcr.io/altinn/gh-runner:latest"
+  runner_image                  = "ghcr.io/altinn/altinn-access-management-frontend/gh-runner:latest"
   runner_cpu                    = "4.0"
   runner_memory                 = "8Gi"
   tags = merge(local.tags, {
